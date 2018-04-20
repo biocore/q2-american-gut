afcdbc339b96ea90274e78d0c1b2d0a498519eec
Daniel McDonald <mcdonadt@colorado.edu>
2018-04-20 15:29:41 -0700

    TST/ENH: added _determine_context to obtain a redbiom context

--- q2_american_gut/_fetch.py
+++ q2_american_gut/_fetch.py
@@ -9,6 +9,61 @@
 import biom
 import pandas as pd
 import skbio
+import redbiom.search
+import redbiom.summarize
+
+
+def _determine_context(processing_type, trim_length):
+    """Determine the processing context in redbiom to use
+
+    Parameters
+    ----------
+    processing_type : str, {deblur, closed-reference}
+        The sequence processing type to look for.
+    trim_length : int
+        The fragment length to look for.
+
+    Notes
+    -----
+    This method is restricted to 16S V4 data.
+
+    Raises
+    ------
+    ValueError
+        If a suitable context could not be found.
+    """
+    contexts = redbiom.summarize.contexts()
+
+    found = None
+    for context in contexts.ContextName:
+        context = context.lower()
+
+        # american gut amplicon data are only 16S v4
+        if '16s' not in context or 'v4' not in context:
+            continue
+
+        # TODO: the release candidate version of the qiita redbiom database
+        # has updated context names which include the reference used.
+        # if we're closed reference, restrict to greengenes as a reference
+        #if processing_type != 'deblur' and 'greengenes' not in context:
+        #    continue
+
+        if processing_type in context and "%dnt" % trim_length in context:
+            found = context
+
+    if found is None:
+        msg = "Cannot find %s-%dnt for 16S data" % (processing_type, trim_length)
+        raise ValueError(msg)
+
+    return found
+
+
+def _assign_taxonomy(table):
+    return pd.DataFrame()
+
+
+def _insert_fragments(table):
+    return skbio.TreeNode()
 
 
 def fetch_amplicon(qiita_study_id: str, processing_type: str, trim_length: int,
@@ -16,4 +71,24 @@ def fetch_amplicon(qiita_study_id: str, processing_type: str, trim_length: int,
                                                           pd.DataFrame,
                                                           pd.DataFrame,
                                                           skbio.TreeNode):
-    pass
+    if not qiita_study_id.isdigit():
+        raise ValueError("ID %s is not a qiita ID" % qiita_study_id)
+
+    if int(qiita_study_id) < 1:
+        raise ValueError("ID %s is not a qiita ID" % qiita_study_id)
+
+    context = _determine_context(processing_type, trim_length)
+
+    query = "where qiita_study_id==%s" % qiita_study_id
+    samples = redbiom.search.metadata_full(query)
+
+    if not samples:
+        raise ValueError("study ID %s has no samples or is not a Qiita ID")
+
+    table, ambiguity_map_tab = redbiom.fetch.data_from_samples(context, samples)
+    md, ambiguity_map_md = redbiom.fetch.metadata_from_samples(context, samples)
+
+    taxonomy = _assign_taxonomy(table)
+    phylogeny = _insert_fragments(table)
+
+    return table, taxonomy, md, phylogeny
--- q2_american_gut/tests/test_fetch.py
+++ q2_american_gut/tests/test_fetch.py
@@ -11,33 +11,61 @@ import unittest
 from qiime2.plugin.testing import TestPluginBase
 
 from q2_american_gut import fetch_amplicon
+from q2_american_gut._fetch import _determine_context
+
+
+class DetermineContextTests(TestPluginBase):
+    package = "q2_american_gut.tests"
+
+    def test_context_without_processing_type(self):
+        proctype = 'something wrong'
+        trim = 113
+
+        msg = "Cannot find %s-%dnt for 16S data" % (proctype, trim)
+        with self.assertRaisesRegex(ValueError, msg):
+            _determine_context(proctype, trim)
+
+    def test_context_does_not_have_trim_length(self):
+        proctype = 'deblur'
+        trim = 42
+
+        msg = "Cannot find %s-%dnt for 16S data" % (proctype, trim)
+        with self.assertRaisesRegex(ValueError, msg):
+            _determine_context(proctype, trim)
+
+    def test_valid_deblur_context(self):
+        proctype = 'deblur'
+        trim = 100
+        ctx = _determine_context(proctype, trim)
+        self.assertIn('deblur', ctx.lower())
+        self.assertIn('100nt', ctx.lower())
+
+    def test_valid_closed_reference_context(self):
+        proctype = 'closed-reference'
+        trim = 100
+        ctx = _determine_context(proctype, trim)
+        self.assertIn('closed-reference', ctx.lower())
+        self.assertIn('100nt', ctx.lower())
 
 
 class TestFetch(TestPluginBase):
     package = "q2_american_gut.tests"
 
     def test_non_existant_study_id(self):
-        id_ = "-1"
-        with self.assertRaisesRegex(ValueError, "study %s not found" % id):
+        id_ = "99999999999999"
+        with self.assertRaisesRegex(ValueError, "has no samples"):
             fetch_amplicon(id_, 'deblur', 100)
 
-    def test_study_without_processing_type(self):
-        id_ = "10313"  # has 18S and ITS
-        proctype = 'deblur'
-        trim = 100
+    def test_study_id_must_be_positive_integer(self):
+        id_ = "-1"
+        with self.assertRaisesRegex(ValueError,
+                                    "ID %s is not a qiita ID" % id_):
+            fetch_amplicon(id_, 'deblur', 100)
 
-        msg = "study %s does not have %s-%dnt 16S data" % (id_, proctype, trim)
-        with self.assertRaisesRegex(ValueError, msg):
-            fetch_amplicon(id_, proctype, trim)
-
-    def test_study_does_not_have_trim_length(self):
-        id_ = "10317"  # has 18S and ITS
-        proctype = 'deblur'
-        trim = 250
-
-        msg = "study %s does not have %s-%dnt 16S data" % (id_, proctype, trim)
-        with self.assertRaisesRegex(ValueError, msg):
-            fetch_amplicon(id_, proctype, trim)
+        id_ = "assd"
+        with self.assertRaisesRegex(ValueError,
+                                    "ID %s is not a qiita ID" % id_):
+            fetch_amplicon(id_, 'deblur', 100)
 
 
 if __name__ == '__main__':
