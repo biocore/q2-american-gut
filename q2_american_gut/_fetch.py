2211bb4a17c6299c8be419eb4f6beb60e86a3246
Nicolai Reeve <nreeve@ucsd.edu>
2018-04-20 16:48:02 -0700

    lil changes

--- q2_american_gut/_fetch.py
+++ q2_american_gut/_fetch.py
@@ -9,11 +9,31 @@
 import biom
 import pandas as pd
 import skbio
+import redboiom
+from redbiom import search, fetch, summarize
 
+
+def determine_context(processing_type, trim_len):
+    
+    pass
 
 def fetch_amplicon(qiita_study_id: str, processing_type: str, trim_length: int,
                    threads: int=1, debug: bool=False) -> (biom.Table,
                                                           pd.DataFrame,
                                                           pd.DataFrame,
                                                           skbio.TreeNode):
+
+    
+    if not qiita_study_id.is_digit():
+        raise ValueError("Qiita study id is not valid")
+
+    
+
+    
     pass
+
+'''
+mission
+find a public study in qiita, like one of the gold star ones
+pull out the deblur 90 nucleotide data for it
+'''
